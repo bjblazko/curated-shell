29a2f7aa49675560297bf9838bed510bf3cadc5f
Timo Böwing <bjblazko@users.noreply.github.com>
2022-06-01 22:56:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+echo 'alias coolshell="curl 'https://raw.githubusercontent.com/bjblazko/gcp-shell/main/coolshell.sh' | /bin/bash"' >>~/.bashrc
