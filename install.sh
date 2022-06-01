e894b1039d5e6f59e191daa4d937046931e576af
Timo Böwing <bjblazko@users.noreply.github.com>
2022-06-01 22:23:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 #
 # Run this with:
-# curl "https://raw.githubusercontent.com/bjblazko/gcp-shell/main/install.sh" | sudo /bin/bash
+# curl "https://raw.githubusercontent.com/bjblazko/gcp-shell/main/install.sh" | /bin/bash
 #
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
