853905b2d52f55a0c72c86de7fe468b39f687e63
Timo Böwing <bjblazko@users.noreply.github.com>
2022-06-02 13:57:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,3 @@ sudo apt-get -yq install zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 curl -o ~/.p10k.zsh "https://raw.githubusercontent.com/bjblazko/gcp-shell/main/p10k.zsh"
-
-echo 'alias cat="batcat"' >> ~/.bashrc
-echo 'alias coolshell="curl 'https://raw.githubusercontent.com/bjblazko/gcp-shell/main/coolshell.sh' | /bin/bash"' >>~/.bashrc
