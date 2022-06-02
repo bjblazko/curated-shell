24a03698b8cb097dd5c05ca9b2f5ed3637717a89
Timo Böwing <bjblazko@users.noreply.github.com>
2022-06-02 14:19:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,14 @@
 #
 
 echo "Install stuff..."
-sudo apt-get -y install \
-   zsh \
-   bat \
-   figlet
+sudo apt-get -y install zsh
 
 [ -d "~/powerlevel10k" ] && rm -rf ~/powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 curl -o ~/.p10k.zsh "https://raw.githubusercontent.com/bjblazko/gcp-shell/main/p10k.zsh"
 curl -o ~/.zrsrc "https://raw.githubusercontent.com/bjblazko/gcp-shell/main/zshrc"
+curl -o ~/.bash_aliases "https://raw.githubusercontent.com/bjblazko/gcp-shell/main/bash_aliases"
+
 
 echo "Done".
 @echo "                      _           _       _          _ _"
