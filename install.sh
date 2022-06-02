4177c9421ff6459aeeeda271133b426d26e13ede
Timo Böwing <bjblazko@users.noreply.github.com>
2022-06-02 09:42:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@
 # curl "https://raw.githubusercontent.com/bjblazko/gcp-shell/main/install.sh" | /bin/bash
 #
 
+echo "Install stuff..."
+sudo apt-get -yq install zsh
+
 [ -d "~/powerlevel10k" ] && rm -rf ~/powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
