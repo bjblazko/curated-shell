dce0d97ea4018adf3097fba522b7aa8a3ab7eb0c
Timo Böwing <bjblazko@users.noreply.github.com>
2022-06-10 14:20:54 +0200

    added fx

--- resources/reinstall-tools.sh
+++ resources/reinstall-tools.sh
@@ -13,5 +13,15 @@ sudo apt install -yq zsh \
 
 # xsv
 XSV_VERSION=$(curl -s "https://api.github.com/repos/BurntSushi/xsv/releases/latest" | grep -Po '"tag_name": "\K[0-9.]+')
-curl -Lo xsv.tar.gz "https://github.com/BurntSushi/xsv/releases/latest/download/xsv-${XSV_VERSION}-x86_64-unknown-linux-musl.tar.gz"
-sudo tar xf xsv.tar.gz -C /usr/local/bin
+XSV_TAR="xsv.tar.gz"
+curl -Lo "$XSV_TAR" "https://github.com/BurntSushi/xsv/releases/latest/download/xsv-${XSV_VERSION}-x86_64-unknown-linux-musl.tar.gz"
+sudo tar xf "$XSV_TAR" -C /usr/local/bin
+rm -f "$XSV_TAR"
+
+# fx
+antonmedv/fx/releases
+FX_VERSION=$(curl -s "https://api.github.com/repos/antonmedv/fx/releases/latest" | grep -Po '"tag_name": "\K[0-9.]+')
+FX_BIN="fx"
+curl -Lo "$FX_BIN" "https://github.com/antonmedv/fx/releases/latest/download/${XSV_VERSION}/fx_linux_amd64"
+chmod +x "$FX_BIN"
+sudo mv "$FX_BIN" /usr/local/bin/
